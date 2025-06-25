ec87084b60c700b9374ea40fe7dff251504e1de2
Sarah Hackett <sarah.hackett@flutteruki.com>
2025-06-25 17:15:38 +0100

    todo interface

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+
+const todos: Todo[] = [];
+
 const btn = document.getElementById("btn");
 const form = document.getElementById("todoform") as HTMLFormElement;
 const input = document.getElementById("todoinput") as HTMLInputElement;
@@ -5,13 +12,22 @@ const list = document.getElementById("todo-list") as HTMLUListElement;
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  const newToDoText = input.value;
-
-  const newLi = document.createElement("li");
-  newLi.textContent = newToDoText;
-  list.append(newLi);
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  newLi.append(checkbox);
+  // in memory
+  const newToDo: Todo = {
+    text: input.value,
+    completed: false,
+  };
+  createToDo(newToDo);
+  todos.push(newToDo);
   input.value = "";
 });
+
+function createToDo(todo: Todo) {
+  // creating new li element
+  const newLi = document.createElement("li");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  newLi.append(todo.text);
+  newLi.append(checkbox);
+  list.append(newLi);
+}
