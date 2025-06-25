4e4cb3b97a8bfbd8d9b75e9cfe61a12c3b50f1ff
Sarah Hackett <sarah.hackett@flutteruki.com>
2025-06-25 16:27:36 +0100

    Type assertions on input

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello World!!");
+const btn = document.getElementById("btn");
+const input = document.getElementById("to-do-input") as HTMLInputElement;
+
+btn?.addEventListener("click", () => {
+  alert(input.value);
+  input.value = "";
+});
